45a30efebe2dca022e0f84f0ce6d8db7ad0ed7eb
termorey <nickfierydragon1995@mail.ru>
2026-02-14 17:19:01 +0300

    chore: added/updated vite ts paths plugin

--- examples/regular-contest/server/vite.config.ts
+++ examples/regular-contest/server/vite.config.ts
@@ -1,17 +1,14 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 import devServer from "@hono/vite-dev-server";
 
 export default defineConfig({
   plugins: [
+    tsConfigPaths(),
     devServer({
       entry: resolve(__dirname, "./src/main.ts"),
     }),
   ],
   server: { host: true },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
 });
